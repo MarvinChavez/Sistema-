0cdcf190e4d2ddd36c7f68650b601096f6ca97fe
MarvinChavez <t513300420@unitru.edu.pe>
2025-02-12 18:50:04 -0500

    finall

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -175,14 +175,6 @@
                 </svg>
                 </i>
             </div>
-            <div class="input-group search-input">
-              <span class="input-group-text" id="search-input">
-                <svg class="icon-18" width="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="11.7669" cy="11.7666" r="8.98856" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></circle>
-                    <path d="M18.0186 18.4851L21.5426 22" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                </svg>
-              </span>
-            </div>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon">
                   <span class="mt-2 navbar-toggler-bar bar1"></span>
@@ -194,6 +186,7 @@
               <ul class="mb-2 navbar-nav ms-auto align-items-center navbar-list mb-lg-0">
                 <li class="nav-item dropdown custom-drop">
                   <a class="py-0 nav-link d-flex align-items-center" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+
                     <div class="caption ms-3 d-none d-md-block ">
                         <h6 class="mb-0 caption-title">{{ Auth::user()->name }}</h6>
                         <p class="mb-0 caption-sub-title">{{ Auth::user()->role->name }}</p>
